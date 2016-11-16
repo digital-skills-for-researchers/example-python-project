a34f3c7aa46d19874a1e5304d38e9f460aa78ab6
Tanya Gray <tanyagray.nz@gmail.com>
2016-11-16 16:53:52 +1300

    Add skeleton for script to clean data

---
+++ clean-data.py
@@ -0,0 +1,35 @@
+'''
+Combines the raw CSV data with the hand-recorded metadata.
+
+Produces a set of file pairs consisting of the [experiment].csv 
+for the data and [experiment].txt for the metadata.
+'''
+
+
+# FUNCTIONS ###################################
+
+def get_row_metadata(row):
+    pass
+
+
+def get_row_data(row):
+    pass
+
+
+'''
+notebook_entry: String (CSV row)
+csv_file: File
+'''
+def combine_row_with_file(notebook_entry, csv_file):
+    pass
+
+
+# PROGRAM #####################################
+
+
+# for row in hand-written metadata
+
+    # get matching csv file
+    
+    # combine row with file
+    
